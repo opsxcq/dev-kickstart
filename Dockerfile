b371c89cd92ef016898f225dfdb5f4909f3f7ed6
OPSXCQ <opsxcq@strm.sh>
2017-10-10 23:26:45 -0300

    initial

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM debian:jessie
+
+LABEL maintainer "opsxcq@strm.sh"
+
+RUN apt-get update && \
+    apt-get upgrade -y && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    qemu qemu-kvm qemu-system-x86 qemu-system \
+    libvirt-bin libvirt-clients virtinst \
+    && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+EXPOSE 5900
+
+VOLUME /src
+WORKDIR /src
+
+COPY main.sh /
+ENTRYPOINT ["/main.sh"]
+CMD ["default"]
+
---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo '[+] Starting libvirtd service'
+service libvirtd start
+
+echo '[+] Installing the operating system'
+virt-install --name linux-build-test --ram 1024 --noreboot \
+    --disk path=./disk.qcow2,size=10 \
+    --vcpus 1 --os-type linux --os-variant generic --network bridge=virbr0,mac=52:54:00:7c:a1:64 \
+    --nographics --console pty,target_type=serial \
+    --initrd-inject=vm.ks --extra-args "ks=file:/vm.ks console=ttyS0,115200n8 serial" \
+    --location=/install.iso
+
+echo '[+] Optimizing the disk image'
+time qemu-img convert -c -O qcow2 disk.qcow2 disk-final.qcow2
+mv disk-final.qcow2 disk.qcow2
